b52175dc10c7820bee61b5753d556b4ab5edc313
eunso <55428135+eunso@users.noreply.github.com>
2019-12-05 17:01:41 +0900

    1301

---
+++ 1301.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct student
+ {
+ 	int ID;
+ 	char name[10];
+ 	double grade;
+ } ;
+ 
+ int main(int argc, char *argv[])
+ {
+ 	struct student s1={24, "EUNSONG",3.5
+ 		return 0;
+	 }
+ 
