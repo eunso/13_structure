d6814037041d52f7e92daa1e4150e05a39c03d20
eunso <55428135+eunso@users.noreply.github.com>
2019-12-05 17:09:20 +0900

    1302

--- 1301.c
+++ 1301.c
@@ -3,16 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-struct student
- {
- 	int ID;
- 	char name[10];
- 	double grade;
- } ;
- 
+
  int main(int argc, char *argv[])
  {
  	struct student s1={24, "EUNSONG",3.5};
+ 		s1. ID=191022;
+ 		strcpy(s1.name, "EUNSONG");
+ 		s1.grade=4.33;
+ 		printf("ID : %d\n",s1,ID);
+ 		printf("name:%s\n",s1.name);
+ 		printf("grade: %f\n",s1.grade);
  		return 0;
 	 }
  
